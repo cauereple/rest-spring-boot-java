599713081f2b8cdf0ba5a7864a75f24377236d91
Cauê Reple <caue-reple@hotmail.com>
2024-05-16 15:37:13 +0200

    Adding the math class

---
+++ 06_ParametersAndExceptionHandler/rest-spring-boot-java/src/test/java/it/com/cursospring/StartupTests.java
@@ -0,0 +1,13 @@
+package it.com.cursospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
