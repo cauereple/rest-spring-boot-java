c13698f3e843a6cbf58cf65b7a61ac0fd1d5b1da
Cauê Reple <caue-reple@hotmail.com>
2024-05-29 23:21:12 +0200

    Adding the second version for the API

--- 05_firstStepsInJavaWithSpringBoot/rest-spring-boot-java/src/main/java/it/com/cursospring/controllers/PersonController.java
+++ 05_firstStepsInJavaWithSpringBoot/rest-spring-boot-java/src/main/java/it/com/cursospring/controllers/PersonController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import it.com.cursospring.model.Person;
+import it.com.cursospring.data.vo.v1.PersonVO;
 import it.com.cursospring.services.PersonServices;
 
 @RestController
@@ -25,27 +25,27 @@ public class PersonController {
 	private PersonServices service;
 	
 	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person findById(@PathVariable(value = "id") Long id) {
+	public PersonVO findById(@PathVariable(value = "id") Long id) {
 		
 		return service.findById(id);
 	}
 	
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<Person> findAll() {
+	public List<PersonVO> findAll() {
 		
 		return service.findAll();
 	}
 	
 	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
 			produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person create(@RequestBody Person person) {
+	public PersonVO create(@RequestBody PersonVO person) {
 		
 		return service.create(person);
 	}
 	
 	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
 			produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person update(@RequestBody Person person) {
+	public PersonVO update(@RequestBody PersonVO person) {
 		
 		return service.update(person);
 	}
--- 05_firstStepsInJavaWithSpringBoot/rest-spring-boot-java/src/main/java/it/com/cursospring/services/PersonServices.java
+++ 05_firstStepsInJavaWithSpringBoot/rest-spring-boot-java/src/main/java/it/com/cursospring/services/PersonServices.java
@@ -1,59 +1,84 @@
 package it.com.cursospring.services;
 
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import it.com.cursospring.data.vo.v1.PersonVO;
+import it.com.cursospring.data.vo.v2.PersonVOV2;
 import it.com.cursospring.exceptions.ResourceNotFoundException;
+import it.com.cursospring.mapper.DozerMapper;
+import it.com.cursospring.mapper.custom.PersonMapper;
 import it.com.cursospring.model.Person;
 import it.com.cursospring.repositories.PersonRepository;
 
 @Service
 public class PersonServices {
 	
-	private static final AtomicLong counter = new AtomicLong();
+//	private static final AtomicLong counter = new AtomicLong();
 	
 	private Logger logger = Logger.getLogger(PersonServices.class.getName());
 	
 	@Autowired
 	PersonRepository repository;
+	
+	@Autowired
+	PersonMapper mapper;
 
-	public List<Person> findAll() {
+	public List<PersonVO> findAll() {
 		
 		logger.info("Finding all people!");
 		
-		return repository.findAll();
+		return DozerMapper.parseListObjects(repository.findAll(), PersonVO.class);
 	}
 	
-	public Person findById(Long id) {
+	public PersonVO findById(Long id) {
 		
 		logger.info("Finding one person!");
 		
-		return repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+		Person entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+		
+		return DozerMapper.parseObject(entity, PersonVO.class);
 	}
 	
-	public Person create(Person person) {
+	public PersonVO create(PersonVO person) {
 		
 		logger.info("Creating one person!");
-
-		return repository.save(person);
+		
+		Person entity = DozerMapper.parseObject(person, Person.class);
+		PersonVO vo = DozerMapper.parseObject(repository.save(entity), PersonVO.class);
+		
+		return vo;
 	}
 	
-	public Person update(Person person) {
+	public PersonVOV2 createV2(PersonVOV2 person) {
+		
+		logger.info("Creating one person with V2!");
+		
+		Person entity = mapper.convertVoToEntity(person);
+		PersonVOV2 vo = mapper.convertEntityToVo(repository.save(entity));
+		
+		return vo;
+	}
+	
+	public PersonVO update(PersonVO person) {
 		
 		logger.info("Updating one person!");
 		
-		Person entity = repository.findById(person.getId()).orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+		Person entity = repository.findById(person.getId())
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
 		
 		entity.setFirstName(person.getFirstName());
 		entity.setLastName(person.getLastName());
 		entity.setAddress(person.getAddress());
 		entity.setGender(person.getGender());
 		
-		return repository.save(person);   
+		PersonVO vo = DozerMapper.parseObject(repository.save(entity), PersonVO.class);
+		
+		return vo;
 	}
 	
 	public void delete(Long id) {
